c17fc8445c4b5026222143b313a36b24a8fa78c9
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-11-16 19:54:00 +0400

    Update huffman_algorithm.py

--- huffman_algorithm.py
+++ huffman_algorithm.py
@@ -1,20 +1,30 @@
-def huffman_algorithm(text: str) -> tuple:
+from heapq import heappush, heappop, heapify
+from collections import defaultdict
 
-    count_char = dict()
+
+def huffman_algorithm(text: str) -> list:
+
+    count_char = defaultdict(int)
     for char in text:
-        if char in count_char:
-            count_char[char] += 1
-        else:
-            count_char[char] = 1
+        count_char[char] += 1
     
-    leaf = sorted(counter_char.items(), key = lambda char: char[1])
-    
-    #processing
+    heap = [[wt, [char, str()]] for char, wt in count_char.items()]
+    heapify(heap)
+    while len(heap) > 1:
+        lo = heappop(heap) #
+        hi = heappop(heap) #
 
-    #test
-    return (counter_char, leaf)
+        for pair in lo[1:]:
+            pair[1] = '0' + pair[1]
+
+        for pair in hi[1:]:
+            pair[1] = '1' + pair[1]
+
+        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+
+    return sorted(heappop(heap)[1:], key = lambda pair: (len(pair[-1]), pair))
 
 
 #test
-output = huffman_algorithm("воровал варвар у варвара варево")
+output = huffman_algorithm("четыре черненьких чумазеньких чертенка чертили черными чернилами чертеж")
 print(output)
