7323863885cc6e618083c751ec9560d4a6885188
fruitourist <70770455+fruitourist@users.noreply.github.com>
2020-11-17 07:42:36 +0400

    Update huffman_algorithm.py

--- huffman_algorithm.py
+++ huffman_algorithm.py
@@ -9,20 +9,20 @@ def huffman_algorithm(text: str) -> list:
         count_char[char] += 1
     
     heap = [[wt, [char, str()]] for char, wt in count_char.items()]
-    heapify(heap)
-    while len(heap) > 1:
+    heapify(heap) #
+    while len(heap) > 1: #
         lo = heappop(heap) #
         hi = heappop(heap) #
 
-        for pair in lo[1:]:
-            pair[1] = '0' + pair[1]
+        for pair in lo[1:]: #
+            pair[1] = '0' + pair[1] #
 
-        for pair in hi[1:]:
-            pair[1] = '1' + pair[1]
+        for pair in hi[1:]: #
+            pair[1] = '1' + pair[1] #
 
-        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:])
+        heappush(heap, [lo[0] + hi[0]] + lo[1:] + hi[1:]) #
 
-    return sorted(heappop(heap)[1:], key = lambda pair: (len(pair[-1]), pair))
+    return sorted(heappop(heap)[1:], key = lambda pair: (len(pair[-1]), pair)) #
 
 
 #test
